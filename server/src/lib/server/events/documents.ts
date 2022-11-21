0965ce7588f6f89729b23e355351a65fa469ce1a
DaanV2 <daanverstraten@hotmail.com>
2022-11-21 20:36:35 +0100

    Adding log messages to on document

--- server/src/lib/server/events/documents.ts
+++ server/src/lib/server/events/documents.ts
@@ -3,5 +3,6 @@ import { TextDocument } from "vscode-languageserver-textdocument";
 
 export async function onDocumentChangedAsync(event: TextDocumentChangeEvent<TextDocument>): Promise<void> {
   //Change on document happened
+  console.log("Document changed");
   return;
 }
